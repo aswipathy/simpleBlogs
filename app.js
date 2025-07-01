5b613346acfa4d797224813ed6f26eed4a4c565e
Aswinee Tripathy <159601731+aswipathy@users.noreply.github.com>
2025-07-01 12:56:29 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+var blogPosts = [];
+app.get('/', (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Listening at port ${port}`);
+});
