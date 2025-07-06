6fbad3e1c67e615e850d781ebe7675b0a70e023c
Aswinee Tripathy <159601731+aswipathy@users.noreply.github.com>
2025-07-06 22:46:20 +0530

    compose works now

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,6 @@
-console.log("index.js loaded!"); // Add this line
-
 const compose = document.getElementById('composeBtn');
 
-if (compose) {
-    console.log("Button found!"); // Add this line
-    compose.addEventListener('click', ()=>{
-        console.log("Button clicked!"); // Add this line
-        window.location.href = '/compose'
-    });
-} else {
-    console.error("Button with ID 'composeBtn' not found!"); // Add this line
-}
+compose.addEventListener('click', ()=>{
+
+    window.location.href = '/compose'
+});
